6f33a55d6cb07306298304a119ccef252036e665
Haolong <chl123.chenhaolong@gmail.com>
2025-07-23 21:45:45 -0400

    chore: initialize project with package.json and essential dependencies

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+import helmet from 'helmet';
+const app = express();
+
+app.use(cors(
+    {
+        origin: process.env.CORS_ORIGIN || '*', // Allow all origins by default
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow specific HTTP methods
+        credentials: true // Allow credentials if needed
+    }
+));
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
