d92ada7457627c83e19f2691610db8cbc869bdfc
Haolong <chl123.chenhaolong@gmail.com>
2025-07-24 04:19:29 -0400

    fix: ensure next middleware is called consistently in syncUser function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,24 +4,26 @@ import { getAuth, clerkClient } from "@clerk/express";
 export const syncUser = async (req, res, next) => {
     try {
         const { userId } = getAuth(req);
+
+        // Check if user already exists
+        let user = await User.findOne({ clerkId: userId });
+        if (user) {
+            return next(); // User exists, proceed to next middleware
+        }
+
         const clerkUser = await clerkClient.users.getUser(userId);
         console.log("Clerk user data:", clerkUser);
         console.log(clerkUser.email_addresses[0]);
         console.log(clerkUser.emailAddresses[0]);
         const { firstName, lastName, imageUrl } = clerkUser;
-        const email = clerkUser.emailAddresses[0].emailAddress;
-
-        // Check if user already exists
-        let user = await User.findOne({ clerkId: userId });
-        if (user) {
-            next(); // User exists, proceed to next middleware
-        }
+        const email = clerkUser.emailAddresses[0]?.emailAddress;
 
         // Create new user
         user = new User({ firstName, lastName, email, clerkId: userId, avatarUrl: imageUrl });
         await user.save();
-        next(); // Proceed to next middleware
+        return next(); // Proceed to next middleware
     } catch (error) {
         res.status(500).json({ message: error.message });
+        next();
     }
 }
